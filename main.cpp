ae60e538d6f2806bbed8195fc628effc6c8d9c94
xlloss <xlloss@gmail.com>
2018-10-24 11:00:56 +0800

    modify main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include "widget.h"
+#include "mainform.h"
 #include <QApplication>
-
+#include "speed_meter.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
-    w.show();
+ //   Widget w;
+ //   w.show();
+    MainForm main;
+    main.show();
 
+//      SpeedMeter s;
+//      s.show();
     return a.exec();
 }
